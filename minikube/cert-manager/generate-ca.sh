8ea5120d2d889e5549be5a1a95f7a35665e6f0d1
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2020-07-28 16:56:04 +0200

    Keep ca.pem for future reference

--- minikube/cert-manager/generate-ca.sh
+++ minikube/cert-manager/generate-ca.sh
@@ -7,4 +7,5 @@ cp /etc/ssl/openssl.cnf $DIR/tmp/openssl-with-ca.cnf
 cat $DIR/openssl-ca.cnf >> $DIR/tmp/openssl-with-ca.cnf
 openssl genrsa -out $DIR/tmp/ca.key 2048
 openssl req -x509 -new -nodes -key $DIR/tmp/ca.key -sha256 -days 3650 -out $DIR/tmp/ca.pem -extensions v3_ca -config $DIR/tmp/openssl-with-ca.cnf
+cp $DIR/tmp/ca.pem $DIR/ca.pem
 open $DIR/tmp/ca.pem
