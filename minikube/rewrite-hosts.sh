18f2a9ff977631c159e1ae2029606b05599a6a15
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2020-07-28 17:35:54 +0200

    Make rewrite-hosts more resilient

--- minikube/rewrite-hosts.sh
+++ minikube/rewrite-hosts.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 MINIKUBE_IP=$(minikube ip)
-for HOSTNAME in $(grep 192.168.64. /etc/hosts | awk '{ print $2 }'); do
+
+echo "" | sed -r 's//' > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    # Linux
+    MINIKUBE_RANGE=$(echo $MINIKUBE_IP | sed -r 's/[0-9]+$//g')
+else
+    # Mac
+    MINIKUBE_RANGE=$(echo $MINIKUBE_IP | sed -E 's/[0-9]+$//g')
+fi
+for HOSTNAME in $(grep $MINIKUBE_RANGE /etc/hosts | awk '{ print $2 }'); do
     echo "sudo hostile set $MINIKUBE_IP $HOSTNAME"
     sudo hostile set $MINIKUBE_IP $HOSTNAME
 done
