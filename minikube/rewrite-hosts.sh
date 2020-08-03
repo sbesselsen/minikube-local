30b0c9bcfda2b1056cf10f5d5fe6efb6c71c05bf
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2020-08-03 16:10:04 +0200

    Update rewrite-hosts to automatically include all ingresses

--- minikube/rewrite-hosts.sh
+++ minikube/rewrite-hosts.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
-MINIKUBE_IP=$(minikube ip)
+#!/usr/bin/env bash
+MINIKUBE_IP=$(minikube ip 2>/dev/null)
+if [ -z $MINIKUBE_IP ]; then
+    echo "No minikube ip."
+    exit 1
+fi
 
 echo "" | sed -r 's//' > /dev/null 2>&1
 if [ $? -eq 0 ]; then
@@ -9,7 +13,17 @@ else
     # Mac
     MINIKUBE_RANGE=$(echo $MINIKUBE_IP | sed -E 's/[0-9]+$//g')
 fi
-for HOSTNAME in $(grep $MINIKUBE_RANGE /etc/hosts | awk '{ print $2 }'); do
-    echo "sudo hostile set $MINIKUBE_IP $HOSTNAME"
+
+OLD_HOSTNAMES=$(grep $MINIKUBE_RANGE /etc/hosts | awk '{ print $2 }' | sort)
+NEW_HOSTNAMES=$(kubectl get ingresses --all-namespaces --context=minikube -o=json | jq -r '.items | map(.spec.rules) | flatten | map(.host)[]' | sort)
+
+ADDED_HOSTNAMES=$(comm -13 <(echo $OLD_HOSTNAMES | tr ' ' '\n') <(echo $NEW_HOSTNAMES | tr ' ' '\n'))
+REMOVED_HOSTNAMES=$(comm -23 <(echo $OLD_HOSTNAMES | tr ' ' '\n') <(echo $NEW_HOSTNAMES | tr ' ' '\n'))
+
+for HOSTNAME in $ADDED_HOSTNAMES; do
     sudo hostile set $MINIKUBE_IP $HOSTNAME
+done
+
+for HOSTNAME in $REMOVED_HOSTNAMES; do
+    sudo hostile remove $HOSTNAME
 done
