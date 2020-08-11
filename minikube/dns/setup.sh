9450b825a800012ab805eba4529c2d7d2c724886
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2020-08-11 07:48:04 +0200

    Fix bugs in the CoreDNS ingress config

--- minikube/dns/setup.sh
+++ minikube/dns/setup.sh
@@ -4,11 +4,4 @@ echo "Making *.ingress.cluster.local accessible to pods through the ingress cont
 DIR=`dirname $0`
 
 kubectl apply -f $DIR/ingress-controller-service.yaml
-
-mkdir -p $DIR/tmp
-kubectl get configmap coredns -n kube-system -o=yaml | \
-    sed $'s/ready/ready\\\n        rewrite name regex (.*)\\\\.ingress\\\\.local ingress-controller.kube-system.svc.cluster.local/g' \
-    > $DIR/tmp/coredns.yml
-
-kubectl apply -f $DIR/tmp/coredns.yml
-rm -rf $DIR/tmp
+kubectl apply -f $DIR/coredns.yaml
