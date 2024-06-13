261b843f49c3222ab3e05d3df2959a789648b656
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2024-06-13 16:50:08 +0200

    Make compatible with Rancher Desktop

--- prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "About to annotate and label ConfigMap kube-system/coredns on cluster $(kubectl config current-context)."
-read -p "Enter to continue, Ctrl+C to cancel..."
-kubectl annotate configmap coredns -n kube-system --overwrite meta.helm.sh/release-name=coredns-patches
-kubectl annotate configmap coredns -n kube-system --overwrite meta.helm.sh/release-namespace=kube-system
-kubectl label configmap coredns -n kube-system --overwrite app.kubernetes.io/managed-by=Helm
